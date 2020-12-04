f1fbdd96560596f14e1ca7236739baf9c77c54df
Pedro Soares <pedrosfig@hotmail.com>
2020-12-04 10:19:31 -0300

    import Matrix::Diagonal

--- R/imports.R
+++ R/imports.R
@@ -2,11 +2,11 @@
 ginv <- MASS::ginv
 
 bdiag <- Matrix::bdiag
+Diagonal <- Matrix::Diagonal
 
 var <- stats::var
 
 sample <- base::sample
 colnames <- base::colnames
 
-is.square.matrix <- matrixcalc::is.square.matrix()
 is.symmetric.matrix <- matrixcalc::is.symmetric.matrix()
