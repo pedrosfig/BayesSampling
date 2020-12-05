c977d8789969e0758a7c6e8e144b58169de56102
Pedro Soares <pedrosfig@hotmail.com>
2020-12-05 12:22:06 -0300

    R matrix

--- R/categorical.R
+++ R/categorical.R
@@ -7,7 +7,6 @@
 #' @param N total size of the population.
 #' @param m k-vector with the prior proportion of each strata. If NULL, sample proportion for each strata will be used (non-informative prior).
 #' @param rho matrix with the prior correlation coefficients between two different units within categories. It must be a symmetric square matrix of dimension k.
-#' @param sigma vector with the prior estimate of variability (standard deviation) within each strata of the population. If NULL, sample variance of each strata will be used.
 #'
 #' @return A list containing the following components: \itemize{
 #' \item \code{est.beta} - BLE of Beta (BLE for the individuals in each strata)
@@ -29,10 +28,10 @@
 #' sigma <- c(1,2,0.5)
 #' N <- c(5,5,3)
 #'
-#' Estimator <- BLE_SSRS(ys,h,N,m,rho,sigma)
+#' Estimator <- BLE_SSRS(ys,h,N,m,rho)
 #' Estimator
 #' @export
-BLE_Categorical <- function(ys, h, N, m=NULL, rho=NULL, sigma=NULL){
+BLE_Categorical <- function(ys, h, N, m=NULL, rho=NULL){
 
   war_1 <- "parameter 'm' (prior proportions) not informed, sample proprotions used in estimations"
   war_2 <- "parameter 'sigma' (prior variability) not informed, sample variance used in estimations"
@@ -64,31 +63,50 @@ BLE_Categorical <- function(ys, h, N, m=NULL, rho=NULL, sigma=NULL){
 
   a <- m
   v <- m*(1-m)    # ver se é necessário !!!!
+  c <- m * (diag(m_ij) - m)
+#  sigma <- sqrt( m*(1-diag(m_ij)) )
+  sigma <- sqrt(v - c)
+
   xs <- diag(k-1)
-  c <- v - sigma^2
-  
   
   
   # Construção do Vs
   
-  Vs_d <- diag(x=sigma^2, nrow = k-1)
+  Vs_d <- diag(sigma^2, nrow = k-1)           # elements in the diagonal
   
   Vs_out <- matrix(0, nrow = k-1, ncol = k-1)
-  for (i in 1:(k-1)) {
+  for (i in 1:(k-1)) {                        # elements outside the diagonal
     for (j in 1:(k-1)) {
       Vs_out[i,j] <- (-m[i])*m_ij[j,i]
     }
   }
-  Vs_out <- Vs_out - Diagonal(k-1, diag(Vs_out))
+  Vs_out <- Vs_out * (1 - diag(nrow = k-1))
   
   Vs <- (1/n)*(Vs_d + Vs_out)
+  #  if( ! is.symmetric.matrix(Vs) ){stop("Vs must be a symmetric matrix. Review parameter 'rho'")}   - proximo teste ja vai dar erro
+  if( ! is.positive.definite(Vs) ){stop("Vs must be a positive-definite matrix. Review parameter 'rho'")}
   
 
   V_nots <- Vs*n/(N-n)  # checar
   
   
   
-  # Construção do R   - FAZER !!!
+  # Construção do R 
+  
+  R_d <- diag(c, nrow = k-1)                  # elements in the diagonal
+  
+  R_out <- matrix(0, nrow = k-1, ncol = k-1)
+  for (i in 1:(k-1)) {                        # elements outside the diagonal
+    for (j in 1:(k-1)) {
+      R_out[i,j] <- m[i] * (m_ij[j,i] - m[j])
+    }
+  }
+  R_out <- R_out * (1 - diag(nrow = k-1))
+  
+  R <- (1/n)*(R_d + R_out)
+  #  if( ! is.symmetric.matrix(R) ){stop("R must be a symmetric matrix. Review parameter 'rho'")}   - proximo teste ja vai dar erro
+  if( ! is.positive.definite(R) ){stop("R must be a positive-definite matrix. Review parameter 'rho'")}
+  
   
   
 
